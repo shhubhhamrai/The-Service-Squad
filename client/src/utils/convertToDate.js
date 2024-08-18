15674880f4a7b9634484d885794386338e15b4d0
Shubham Rai <140876805+shhubhhamrai@users.noreply.github.com>
2024-08-18 15:39:12 +0530

    Updates

--- client/src/utils/convertToDate.js
+++
@@ -1,8 +0,0 @@
-const convertDate = string => {
-  var d = string;
-  d = new Date(d);
-  d = d.getMonth() + 1 + "/" + d.getDate() + "/" + d.getFullYear();
-  return d;
-};
-
-export default convertDate;
--- model/Subscribe.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, Sequelize) => {
-  return sequelize.define("subscribe", {
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false
-    }
-  });
-};
